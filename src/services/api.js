90dbf367ccf4e97f41fdaa35be08708196a02faa
DebeshAcharya72 <debeshacharya72@gmail.com>
2025-12-27 21:41:36 +0530

    done

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
-const API_BASE = "http://localhost:8000";
-// const API_BASE = import.meta.env.VITE_API_BASE_URL;
+// const API_BASE = "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 // Token management
 let authToken = localStorage.getItem("authToken") || "";
